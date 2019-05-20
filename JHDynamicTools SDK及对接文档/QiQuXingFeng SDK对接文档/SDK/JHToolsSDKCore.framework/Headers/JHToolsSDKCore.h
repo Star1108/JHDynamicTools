1927162d74c645d71406d23fb33e8e10ca500a0d
Star <1453468914@qq.com>
2019-05-20 10:05:58 +0800

    no message

---
+++ JHDynamicTools SDK及对接文档/QiQuXingFeng SDK对接文档/SDK/JHToolsSDKCore.framework/Headers/JHToolsSDKCore.h
@@ -0,0 +1,28 @@
+//
+//  JHToolsSDKCore.h
+//  JHToolsSDKCore
+//
+//  Created by Star on 2018/3/17.
+//  Copyright © 2018年 Star. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JHToolsSDKCore.
+FOUNDATION_EXPORT double JHToolsSDKCoreVersionNumber;
+
+//! Project version string for JHToolsSDKCore.
+FOUNDATION_EXPORT const unsigned char JHToolsSDKCoreVersionString[];
+
+#import "JHToolsSDK.h"
+#import "JHToolsSDKProxy.h"
+#import "JHToolsUser.h"
+#import "JHToolsPay.h"
+#import "JHToolsAnalytics.h"
+#import "JHToolsShare.h"
+#import "JHToolsPush.h"
+#import "JHToolsSDK+AppDelegate.h"
+
+// In this header, you should import all the public headers of your framework using statements like #import <JHToolsSDKCore/PublicHeader.h>
+
+
